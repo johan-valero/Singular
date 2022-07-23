b0694d0c582362075037d59a240897df298faaa3
johan-valero <valero_johan@hotmail.fr>
2022-07-23 19:41:27 +0200

    update header with all links and add logo

--- app/views/header.php
+++ app/views/header.php
@@ -89,8 +89,8 @@
                 <div class="row">
                     <div class="col-lg-6">
                         <ul class="tn-left">
-                            <li><i class="fa fa-phone"></i> (12) 345 67890</li>
-                            <li><i class="fa fa-envelope"></i> info.colorlib@gmail.com</li>
+                            <li><i class="fa fa-phone"></i>06 27 89 02 54</li>
+                            <li><i class="fa fa-envelope"></i> <?= EMAIL_WEBSITE?></li>
                         </ul>
                     </div>
                     <div class="col-lg-6">
@@ -101,16 +101,32 @@
                                 <a href="#"><i class="fa fa-tripadvisor"></i></a>
                                 <a href="#"><i class="fa fa-instagram"></i></a>
                             </div>
-                            <a href="#" class="bk-btn">Booking Now</a>
+                            <a href="#" class="bk-btn">Réserver</a>
                             <div class="language-option">
-                                <img src="<?=ASSETS?>img/flag.jpg" alt="">
-                                <span>EN <i class="fa fa-angle-down"></i></span>
-                                <div class="flag-dropdown">
-                                    <ul>
-                                        <li><a href="#">Zi</a></li>
-                                        <li><a href="#">Fr</a></li>
-                                    </ul>
-                                </div>
+                                <?php
+                                    if (!isset($data['user_data'])){
+                                        echo '
+                                            <i class="fa fa-user"></i>
+                                            <span> Compte <i class="fa fa-angle-down"></i></span>
+                                            <div class="flag-dropdown">
+                                                <ul>
+                                                    <li><a href="'.ROOT.'signup">Inscription</a></li>
+                                                    <li><a href="'.ROOT.'login">Connexion</a></li>
+                                                </ul>
+                                            </div>
+                                            ';}
+                                    else{
+                                        echo '
+                                        <i class="fa fa-user"></i>
+                                        <span> '. ucwords($data["user_data"]->firstname)," ",ucwords($data["user_data"]->name).' <i class="fa fa-angle-down"></i></span>
+                                        <div class="flag-dropdown">
+                                            <ul>
+                                                <li><a href="'.ROOT.'profil">Profil</a></li>
+                                                <li><a href="'.ROOT.'logout">Déconnexion</a></li>
+                                            </ul>
+                                        </div>
+                                        ';}       
+                                ?>
                             </div>
                         </div>
                     </div>
@@ -122,8 +138,8 @@
                 <div class="row">
                     <div class="col-lg-2">
                         <div class="logo">
-                            <a href="./index.html">
-                                <img src="<?=ASSETS?>img/logo.png" alt="">
+                            <a href="<?=ROOT?>home">
+                                <img style="margin-top:-25px;height:80px;" src="<?=ASSETS?>img/logo/logow.png" alt="">
                             </a>
                         </div>
                     </div>
@@ -131,18 +147,10 @@
                         <div class="nav-menu">
                             <nav class="mainmenu">
                                 <ul>
-                                    <li class="active"><a href="./index.html">Home</a></li>
-                                    <li><a href="./rooms.html">Rooms</a></li>
-                                    <li><a href="./about-us.html">About Us</a></li>
-                                    <li><a href="./pages.html">Pages</a>
-                                        <ul class="dropdown">
-                                            <li><a href="./room-details.html">Room Details</a></li>
-                                            <li><a href="./blog-details.html">Blog Details</a></li>
-                                            <li><a href="#">Family Room</a></li>
-                                            <li><a href="#">Premium Room</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="./blog.html">News</a></li>
+                                    <li class="active"><a href="./index.html">Accueil</a></li>
+                                    <li><a href="./rooms.html">Logements</a></li>
+                                    <li><a href="./about-us.html">A propos</a></li>
+                                    <li><a href="./blog.html">Blog</a></li>
                                     <li><a href="./contact.html">Contact</a></li>
                                 </ul>
                             </nav>
