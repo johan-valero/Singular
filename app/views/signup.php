812c3b91819deaaa324030077531d2db404cf404
johan-valero <valero_johan@hotmail.fr>
2022-07-28 17:32:04 +0200

    Update signup.php

--- app/views/signup.php
+++ app/views/signup.php
@@ -1,13 +1,12 @@
 <?php $this->view("header", $data); ?>	
 	<section>
 		<div class="d-lg-flex half">
-			<div class="bg order-1 order-md-2" style="background-image: url('<?=ASSETS?>img/slider/1.jpg');"></div>
+			<div class="bg order-1 order-md-2" style="background-image: url('<?=ASSETS?>img/slider/2.jpg');"></div>
 			<div class="contents order-2 order-md-1">
 				<div class="container">
 					<div class="row align-items-center justify-content-center">
 						<div class="col-md-7">
 							<h3 style="margin: 0 0 0 0;">Inscription à <strong>Singular</strong></h3>
-							<!-- <p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p> -->
 							<span style="color:#cd701c;line-height:17px;"><?php check_error() ?></span>
 							<form method="POST">
 							<div class="form-group first">
