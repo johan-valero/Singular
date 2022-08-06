00f4ec336e8a10ae5e8a9ba09a652fe4847a3c8c
johan-valero <valero_johan@hotmail.fr>
2022-08-06 18:14:53 +0200

    Update index.php

--- app/views/index.php
+++ app/views/index.php
@@ -123,7 +123,7 @@
                                             <div class="con"> <span class="category">
                                                     <a style="font-size:10px;line-height:14px;" href="'.ROOT.'rooms/category/'.$category->name_category.'">'.$category->description_category.'</a>
                                                 </span>
-                                                <h5><a href="'.ROOT.'rooms/category/'.$category->name_category.'">'.$category->name_category.'</a></h5>
+                                                <h5><a href="'.ROOT.'rooms/'.$category->name_category.'">'.$category->name_category.'</a></h5>
                                             </div>
                                         </div>
                                     ';
