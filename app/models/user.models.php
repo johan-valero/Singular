36e013acd2e323905692bc0b7fbe42b5c6d48ac0
johan-valero <valero_johan@hotmail.fr>
2022-08-11 16:20:19 +0200

    add comments on model users

--- app/models/user.models.php
+++ app/models/user.models.php
@@ -1,7 +1,7 @@
 <?php
 
 class User{
-
+    // Variable privé qui stockera les erreurs
     private $error = "";
 
     // Fonction inscription du user
@@ -129,7 +129,6 @@ class User{
         return $DB->read("select * from users");
     }
 
-
     // Fonction pour générer une url propre aux users
     private function get_random_string_max($length){
         $array = array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
@@ -190,7 +189,7 @@ class User{
         return $data[0];
     } 
 
-    // Getter pour d'un user en fonction de l'id
+    // Fonction pour le mot de passe oublié
     public function forget($POST){
         $db = Database::newInstance();
 
